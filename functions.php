afbf48f07366f38e60fe0741666ec28bd0d5a24d
Jayden Major <jayden@jaydenmajor.com>
2025-12-15 10:16:21 +1000

    Updater Config

--- functions.php
+++ functions.php
@@ -60,7 +60,4 @@ add_action('init', 'tv_videos', 0);
 require 'plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 $myUpdateChecker = PucFactory::buildUpdateChecker('https://github.com/BizInk/arc-child-taxandtrust',__FILE__,'arc-child-taxandtrust');
-// Set the branch that contains the stable release.
 $myUpdateChecker->setBranch('main');
-// Using a private repository, specify the access token 
-$myUpdateChecker->setAuthentication('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');
